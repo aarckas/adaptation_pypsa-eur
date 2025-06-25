97a0d7674c200bdefa0ec8939b19236bb9f01eb4
lderoo <lieven.deroo@externel.be>
2025-06-25 13:18:34 +0200

    mock snakemake adj-.

--- scripts/prepare_network.py
+++ scripts/prepare_network.py
@@ -292,8 +292,8 @@ if __name__ == "__main__":
         snakemake = mock_snakemake(
             "prepare_network",
             configfiles="config/baltic/baltic_test.yaml",
-            clusters="80",
-            opts="onwind+p0.3",
+            clusters="100",
+            opts="Co2L0",
         )
     configure_logging(snakemake)  # pylint: disable=E0606
     set_scenario_config(snakemake)
