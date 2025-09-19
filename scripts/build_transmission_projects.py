4d88e39da292a88ec3a108f324952126008b0fc0
lderoo <lieven.deroo@externel.be>
2025-09-19 15:05:47 +0200

    mock snakemake

--- scripts/build_transmission_projects.py
+++ scripts/build_transmission_projects.py
@@ -605,7 +605,7 @@ if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
 
-        snakemake = mock_snakemake("build_transmission_projects", configfiles="config/baltic/baltic_test.yaml")
+        snakemake = mock_snakemake("build_transmission_projects", configfiles="config/baltic/baltic_sec.yaml")
     configure_logging(snakemake)
     set_scenario_config(snakemake)
 
